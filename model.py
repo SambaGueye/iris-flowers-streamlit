7a05e85394040593fb104ff92ca78f652256876c
SambaGueye <59372751+SambaGueye@users.noreply.github.com>
2022-08-07 01:02:58 +0200

    change model

--- model.py
+++ model.py
@@ -13,9 +13,9 @@ if __name__=='__main__':
     # print(iris.target_names)
     X_train, X_test, y_train, y_test = train_test_split(iris.data, iris.target, test_size=0.2, random_state=0)
     
-    model = xgb(n_estimators=100, learning_rate=1.0, max_depth=1, random_state=0).fit(X_train, y_train)
+    model = xgb().fit(X_train, y_train)
 
     predictions = model.predict(X_test)
 
-    print(model.score(X_test, y_test))
+    # print(model.score(X_test, y_test))
     joblib.dump(model, "xgb_model.sav")
